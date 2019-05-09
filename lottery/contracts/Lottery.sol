7bbc88700b34372e05a80dc7e98d0e3f58c44072
Sanjay S B <sanjay_b@persistent.com>
2019-05-09 16:50:15 +0530

    add the code from remix.

--- lottery/contracts/Lottery.sol
+++ lottery/contracts/Lottery.sol
@@ -1,14 +1,35 @@
 pragma solidity ^0.4.17;
 
-contract Inbox {
-    string public message;
-
-    function Inbox(string initialMessage) public{
-        message = initialMessage;
+contract Lottery {
+    address public manager;
+    address[] public players;
+    
+    function Lottery() public {
+        manager = msg.sender;
     }
-
-    function setMessage(string newMessage) public{
-
-        message = newMessage;
+    
+    function enter() public payable {
+        require(msg.value > .01 ether);
+        players.push(msg.sender);
     }
+    
+    function random() private view returns (uint) {
+        return uint(keccak256(block.difficulty, now, players));
+    }
+    
+    function pickWinner() public restricted {
+        uint index = random() % players.length;
+        players[index].transfer(this.balance);
+        players = new address[](0);
+    }
+    
+    modifier restricted() {
+        require(msg.sender == manager);
+        _;
+    }
+    
+    function getPlayers() public view returns (address[]) {
+        return players;
+    }
+    
 }
